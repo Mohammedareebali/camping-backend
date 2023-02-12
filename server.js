74a031e1224c375f9dcd47f08300353ca48e350b
Mohammedareebali <mohammedareebali740@gmail.com>
2023-02-12 13:41:34 +0000

    fix:typescript setup added tsconfig.json file for typescript to compile .ts to .js file .

---
+++ server.js
@@ -0,0 +1,11 @@
+"use strict";
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello from Express!');
+});
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+//# sourceMappingURL=server.js.map
