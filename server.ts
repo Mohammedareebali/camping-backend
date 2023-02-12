09a74fba0cca26b37100d580eb7958c66f02379b
Mohammedareebali <mohammedareebali740@gmail.com>
2023-02-12 14:20:12 +0000

    feat: firebase setup setting up firebase with jwt and bycrypt and adding .env file for firebase

--- server.ts
+++ server.ts
@@ -1,5 +1,22 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+import * as firebase from "firebase/app";
+import 'firebase/auth';
+
 const express = require('express');
 const app = express();
+
+// Initialize Firebase
+ firebase.initializeApp({
+  apiKey: "API_KEY",
+  authDomain: "AUTH_DOMAIN",
+  projectId: "PROJECT_ID",
+  storageBucket: "STORAGE_BUCKET",
+  messagingSenderId: "MESSAGING_SENDER_ID",
+  appId: "APP_ID"
+})
+
+
 
 app.get('/', (req: any, res: { send: (arg0: string) => void; }) => {
   res.send('Hello from Express!');
